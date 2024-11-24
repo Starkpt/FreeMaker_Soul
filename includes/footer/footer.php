fadbe519efaf82accba0cb3877118a013580ce70
Frederico Ramos <frederico.jlr@gmail.com>
2024-11-24 17:31:53 +0000

    add socials to footer

--- includes/footer/footer.php
+++ includes/footer/footer.php
@@ -11,22 +11,14 @@
         <!-- Right -->
         <div>
             <a href="" class="me-4 text-reset">
-                <i class="fab fa-facebook-f"></i>
+                <img
+                    src="/assets/imgs/icons/facebook.svg"
+                    alt="Facebook" />
             </a>
             <a href="" class="me-4 text-reset">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-                <i class="fab fa-github"></i>
+                <img
+                    src="/assets/imgs/icons/instagram.svg"
+                    alt="Instagram" />
             </a>
         </div>
         <!-- Right -->
@@ -40,10 +32,9 @@
                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                     <!-- Content -->
                     <h6 class="text-uppercase fw-bold mb-4">
-                        <!-- <img
-                            class="failory-logo-image logo"
-                            src="/assets/imgs/logos/logo.png"
-                            alt="LogoFreemaker_soul" /> -->
+                        <img
+                            src="/assets/imgs/icons/squirrel.svg"
+                            alt="LogoFreemaker_soul" />
                         <i class="fas fa-gem me-3"></i>
                         <?= SITE_NAME ?>
                     </h6>
